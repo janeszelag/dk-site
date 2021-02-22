64f09b00480a808fe36ce28ad1c6c6521cb4baa5
katherine szelag <katherine.szelag@mail.utoronto.ca>
2021-02-22 17:28:49 -0500

    fix: spelling

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
         Extensive background in the commercial world having collaborated with some of the top clients in the country including McDonald’s, Google, Pizza Pizza, Koodo, SickKids and more.
         <br />
         <br />
-        My surreal and off-beat film projects film projects have been featured in many reputable film festivals including the Boston Underground Film Festival,
+        My surreal and off-beat film projects have been featured in many reputable film festivals including the Boston Underground Film Festival,
         the Calgary Underground Film Festival and the LA Film Forum. I am graduate of the Sheridan Media Arts/ Film & Television program.
         </Paragraph>
         <InfoDiv>
